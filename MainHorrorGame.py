019011e2ede7af75d0f65cb5a7c06ea87e061619
AdamBachchan <118978420+AdamBachchan@users.noreply.github.com>
2022-11-29 19:46:48 +0100

    Create MainHorrorGame.py

---
+++ MainHorrorGame.py
@@ -0,0 +1,14 @@
+##- 𝐇𝐓𝐑𝐀𝐄
+import testingmodule1
+
+## WORKS PERFECT
+testingmodule1.start()
+
+## WORKS PERFECT
+testingmodule1.secondScenario()
+
+## NEEDS WORK
+testingmodule1.upstairs()
+
+## HAVE NOT STARTED
+testingmodule1.blueRedDoors()
